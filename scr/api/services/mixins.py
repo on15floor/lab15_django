c251f658d3336cbb39e8fa20ba8c712a70fe02fc
on15floor <on15floor@gmail.com>
2021-11-05 21:53:02 +0300

    Refactor

---
+++ scr/api/services/mixins.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+from django.core.exceptions import PermissionDenied
+
+
+class BaseMixin:
+    def __init__(self):
+        self.token = ''
+        self.res = {}
+        self.uri = ''
+        self.status = 'success'
+
+    def check_token(self, request):
+        if 'token' in request.GET:
+            self.token = request.GET['token']
+            self.uri = request.path
+            if self.token == settings.API_QUERY_TOKEN:
+                return
+        self.status = 'error'
+        self.save_log_to_mongodb({'permission_denied': 'wrong token'})
+        raise PermissionDenied
+
+    def save_log_to_mongodb(self, res):
+        log = {
+            'status': self.status,
+            'token': self.token,
+            'uri': self.uri
+        }
+        self.res = {**log, **res}
+        print(self.res)
+        return self.res
--- scr/api/views.py
+++ scr/api/views.py
@@ -1,40 +1,11 @@
 from datetime import datetime
 
-from django.conf import settings
-from django.core.exceptions import PermissionDenied
 from django.http import JsonResponse
 from django.views import View
 
 from birthdays.models import Birthday
+from .services.mixins import BaseMixin
 from .services.telegram import TBot
-
-
-class BaseMixin:
-    def __init__(self):
-        self.token = ''
-        self.res = {}
-        self.uri = ''
-        self.status = 'success'
-
-    def check_token(self, request):
-        if 'token' in request.GET:
-            self.token = request.GET['token']
-            self.uri = request.path
-            if self.token == settings.API_QUERY_TOKEN:
-                return
-        self.status = 'error'
-        self.save_log_to_mongodb({'permission_denied': 'wrong token'})
-        raise PermissionDenied
-
-    def save_log_to_mongodb(self, res):
-        log = {
-            'status': self.status,
-            'token': self.token,
-            'uri': self.uri
-        }
-        self.res = {**log, **res}
-        print(self.res)
-        return self.res
 
 
 class GetBirthdays(BaseMixin, View):
