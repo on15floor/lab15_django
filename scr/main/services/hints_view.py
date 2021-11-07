83a4632b63510dd0a238fc83882c7cd471c48342
on15floor <on15floor@gmail.com>
2021-11-07 17:59:44 +0300

    Fix view of markdown files in prod

--- scr/main/services/hints_view.py
+++ scr/main/services/hints_view.py
@@ -1,7 +1,13 @@
+import os.path
+
 import markdown
+from django.templatetags.static import static
+from django.conf import settings
 
 
 def get_markdown(file_name):
-    data_file = f'main/static/main/docs/{file_name}.md'
+    data_file = static(f'main/static/main/docs/{file_name}.md')
+    data_file = os.path.join(settings.BASE_DIR, 'main/static/main/docs', f'{file_name}.md')
+    print(data_file)
     with open(data_file) as f:
         return markdown.markdown(f.read())
