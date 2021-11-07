93be90accb7e31de232cfa5f4fbbf8859278ce7c
on15floor <on15floor@gmail.com>
2021-11-07 17:31:22 +0300

    Fix static files on prod

--- scr/config/urls.py
+++ scr/config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.static import serve as mediaserv
+from django.conf.urls import url
+from config import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
@@ -9,3 +13,8 @@ urlpatterns = [
     path('', include('birthdays.urls')),
     path('', include('api.urls'))
 ]
+
+if not settings.DEBUG:
+    urlpatterns += [
+        url(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$', mediaserv, {'document_root': settings.STATIC_ROOT}),
+    ]
