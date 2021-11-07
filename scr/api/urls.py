5b8298dd598311e6d518d94ef8ee98e85fa05383
on15floor <on15floor@gmail.com>
2021-11-07 18:13:16 +0300

    Minor url fix

--- scr/api/urls.py
+++ scr/api/urls.py
@@ -5,5 +5,5 @@ from .views import GetBirthdays, GetBegetNews, GetApptimeSales
 urlpatterns = [
     path('api/v1.0/get_birthdays', GetBirthdays.as_view(), name='api_get_birthdays'),
     path('api/v1.0/get_beget_news', GetBegetNews.as_view(), name='api_get_beget_news'),
-    path('api/v1.0/get_ios_sale/', GetApptimeSales.as_view(), name='api_get_apptime_sale'),
+    path('api/v1.0/get_ios_sale', GetApptimeSales.as_view(), name='api_get_apptime_sale'),
 ]
