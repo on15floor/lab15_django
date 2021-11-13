b6cc952435195f0cfbec237179af72440c41b48c
on15floor <on15floor@gmail.com>
2021-11-13 21:54:59 +0300

    Crontab added to dashboard: get_tasks, del, start/stop

--- scr/dashboard/urls.py
+++ scr/dashboard/urls.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 
-from .views import GitLogView, MongoLogView
+from .views import GitLogView, MongoLogView, CronTabView, CronTabTaskDel, CronTabTaskStart, CronTabTaskStop
 
 urlpatterns = [
     path('dashboard/gitlog/', login_required(GitLogView.as_view()), name='dashboard_gitlog'),
     path('dashboard/mongo_log/<state>', login_required(MongoLogView.as_view()), name='dashboard_mongolog'),
+    path('dashboard/crontab/', login_required(CronTabView.as_view()), name='dashboard_crontab'),
+    path('dashboard/crontab/<task_id>/del', login_required(CronTabTaskDel.as_view()), name='dashboard_crontab_del'),
+    path('dashboard/crontab/<task_id>/start', login_required(CronTabTaskStart.as_view()), name='dashboard_crontab_start'),
+    path('dashboard/crontab/<task_id>/stop', login_required(CronTabTaskStop.as_view()), name='dashboard_crontab_stop'),
 ]
--- scr/dashboard/views.py
+++ scr/dashboard/views.py
@@ -1,9 +1,10 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
 from .services.get_gitlog import get_gitlog
 from .services.mongo import MongoDB
+from .services.beget_crontab import Crontab
 
 
 class GitLogView(View):
@@ -26,3 +27,36 @@ class MongoLogView(View):
         else:
             raise Http404()
         return render(request, self.template, context={'logs': list(logs)})
+
+
+class CronTabView(View):
+    """Страница CronTab"""
+    template = 'dashboard/crontab.html'
+    crontab = Crontab()
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template, context={'crontab_tasks': self.crontab.tasks_get()})
+
+
+class CronTabTaskDel(View):
+    """Удалить задачу CronTab"""
+    @staticmethod
+    def get(request, task_id, *args, **kwargs):
+        Crontab().task_del(task_id)
+        return redirect('dashboard_crontab')
+
+
+class CronTabTaskStart(View):
+    """Запустить задачу CronTab"""
+    @staticmethod
+    def get(request, task_id, *args, **kwargs):
+        Crontab().task_change_state(task_id, 0)
+        return redirect('dashboard_crontab')
+
+
+class CronTabTaskStop(View):
+    """Остановить задачу CronTab"""
+    @staticmethod
+    def get(request, task_id, *args, **kwargs):
+        Crontab().task_change_state(task_id, 1)
+        return redirect('dashboard_crontab')
