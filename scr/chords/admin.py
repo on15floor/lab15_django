0012d14d8f4a9cf1d9653908f5149fc6eda3a2b5
on15floor <on15floor@gmail.com>
2021-11-18 22:08:58 +0300

    Add chords images to static files

--- scr/chords/admin.py
+++ scr/chords/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Song
+
+
+@admin.register(Song)
+class PostAdmin(admin.ModelAdmin):
+    model = Song
+    list_display = ('instrument', 'song_name', 'date', 'id')
+    search_fields = ('instrument', 'song_name', 'song_text')
--- scr/chords/models.py
+++ scr/chords/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 from django.utils.timezone import now
 
+ukulele_chords = {'A', 'A7', 'Am', 'Am7', 'Adim', 'Aaug', 'A6', 'Amaj7', 'A9',
+                  'Ab', 'Ab7', 'Abm', 'Abm7', 'Abdim', 'Abaug', 'Ab6', 'Abmaj7', 'Ab9',
+                  'B', 'B7', 'Bm', 'Bm7', 'Bdim', 'Baug', 'B6', 'Bmaj7', 'B9',
+                  'Bb', 'Bb7', 'Bbm', 'Bbm7', 'Bbdim', 'Bbaug', 'Bb6', 'Bbmaj7', 'Bb9',
+                  'C', 'C7', 'Cm', 'Cm7', 'Cdim', 'Caug', 'C6', 'Cmaj7', 'A9',
+                  'D', 'D7', 'Dm', 'Dm7', 'Ddim', 'Daug', 'D6', 'Dmaj7', 'D9',
+                  'Db', 'Db7', 'Dbm', 'Dbm7', 'Dbdim', 'Dbaug', 'Db6', 'Dbmaj7', 'Db9',
+                  'E', 'E7', 'Em', 'Em7', 'Edim', 'Eaug', 'E6', 'Emaj7', 'E9',
+                  'Eb', 'Eb7', 'Ebm', 'Ebm7', 'Ebdim', 'Ebaug', 'Eb6', 'Ebmaj7', 'Eb9',
+                  'F', 'F7', 'Fm', 'Fm7', 'Fdim', 'Faug', 'F6', 'Fmaj7', 'F9',
+                  'G', 'G7', 'Gm', 'Gm7', 'Gdim', 'Gaug', 'G6', 'Gmaj7', 'G9',
+                  'Gb', 'Gb7', 'Gbm', 'Gbm7', 'Gbdim', 'Gbaug', 'Gb6', 'Gbmaj7', 'Gb9'}
+guitar_chords = {'A', 'Am', 'A7', 'B', 'Bm', 'B7', 'C', 'Cm', 'C7', 'D', 'Dm', 'D7',
+                 'E', 'Em', 'E7', 'F', 'Fm', 'F7', 'G', 'Gm', 'G7'}
+
 
 class Song(models.Model):
     """ Модель песни """
@@ -16,3 +31,11 @@ class Song(models.Model):
 
     def __str__(self):
         return f'[{self.instrument}] {self.song_name}'
+
+    @property
+    def get_ukulele_chords(self):
+        return set(self.song_text.split()) & ukulele_chords
+
+    @property
+    def get_guitar_chords(self):
+        return set(self.song_text.split()) & guitar_chords
